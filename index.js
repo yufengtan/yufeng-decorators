b13cc188ddafc1381f7a01a17cc04f9f001b8a80
tanyufeng <fengzi_tan@126.com>
2019-01-17 15:49:11 +0800

    Signed-off-by: tanyufeng <fengzi_tan@126.com>

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const GetMapping = (path) => {
+	return (target, name, descriptor) => {
+		const oldValue = descriptor.value;
+		descriptor.value = function() {
+			console.log(`GetMapping ${path}`)
+			return app.get(path, (req, res, next) => {
+				let result = oldValue.bind(this)(req, res, next);
+				if (result) {
+					if (typeof result === 'object') {
+						res.json(result);
+					} else {
+						res.send(result)
+					}
+				} else {
+					next();
+				}
+			});
+		}
+		descriptor.enumerable = true;
+		return descriptor;
+	}
+}
+
+const RestController = (target) => {
+	var cc = new target;
+	for (let key in target.prototype) {
+		cc[key]();
+	}
+}
+
+module.exports = {
+	app,
+	GetMapping,
+	RestController
+};
